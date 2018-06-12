b88b76e30bf2ed39b32117294b7d753dd7146aee
Fredrik Tuomas <fredrik.tuomas@eniro.com>
2018-06-12 16:32:58 +0200

    Add support for Home Assistant

--- main.go
+++ main.go
@@ -20,6 +20,26 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+//HomeAssistant data
+type HomeAssistantData struct {
+	EventType string    `json:"event_type"`
+	EventData EventData `json:"event_data"`
+}
+
+type EventData struct {
+	EntityID string     `json:"entity_id"`
+	OldState EventState `json:"old_state"`
+	NewState EventState `json:"new_state"`
+}
+
+type EventState struct {
+	EntityID    string                 `json:"entity_id"`
+	State       string                 `json:"state"`
+	Attributes  map[string]interface{} `json:"attributes"`
+	LastChanged *time.Time             `json:"last_changed"`
+	LastUpdated *time.Time             `json:"last_updated"`
+}
+
 //DomoticzsData data from Domoticz
 type DomoticzsData struct {
 	ID      string `json:"id"`
@@ -36,12 +56,12 @@ type VaderData struct {
 }
 
 type Configuration struct {
-	DBHost   string
-	DBUser   string
-	DBPasswd string
-	MQTTHost string
-	Group    string
-	Sensor   []ConfigurationSensor
+	DataFormat string
+	RemoteHost string
+	RemoteApp  string
+	MQTTHost   string
+	Group      string
+	Sensor     []ConfigurationSensor
 }
 
 type ConfigurationSensor struct {
@@ -64,19 +84,6 @@ func init() {
 
 func main() {
 	flag.Parse()
-	/*
-		c2 := Configuration{}
-		c2.DBHost = "TEST"
-		c2.Sensor = make([]ConfigurationSensor, 0)
-
-		c2.Sensor = append(c2.Sensor, ConfigurationSensor{Domoticz: "A", Vader: "B"})
-		c2.Sensor = append(c2.Sensor, ConfigurationSensor{Domoticz: "C", Vader: "D"})
-
-		b, _ := yaml.Marshal(c2)
-		fmt.Printf("%s", string(b))
-
-		os.Exit(0)
-	*/
 	logger, level, err := createLogger()
 
 	if err != nil {
@@ -132,40 +139,85 @@ func main() {
 
 	logger.Debug("Connected to server")
 
-	// Subscribe to topics.
-	err = cli.Subscribe(&client.SubscribeOptions{
-		SubReqs: []*client.SubReq{
-			&client.SubReq{
-				TopicFilter: []byte("domoticz/out"),
-				QoS:         mqtt.QoS0,
-				// Define the processing of the message handler.
-				Handler: func(topicName, message []byte) {
-					dd := &DomoticzsData{}
-					err := json.Unmarshal(message, dd)
-					if err != nil {
-						logger.Error("Failed to unmarshall data", zap.Error(err))
-					}
-					name, stype := findVaderTypes(dd.Name)
-					valueFloat, err := strconv.ParseFloat(dd.Svalue1, 64)
-					if err != nil {
-						logger.Error("Could not parse float", zap.String("value", dd.Svalue1))
-						return
-					}
-					if name != "" {
-						vd := VaderData{
-							Name:       name,
-							Value:      valueFloat,
-							SensorType: stype,
-							Timestamp:  fmt.Sprintf("%d", time.Now().Unix()*1000),
+	if config.DataFormat == "hass" {
+		logger.Debug("Dataformat Home Assistant")
+		// Subscribe to topics.
+		err = cli.Subscribe(&client.SubscribeOptions{
+			SubReqs: []*client.SubReq{
+				&client.SubReq{
+					TopicFilter: []byte("Tvistevägen"),
+					QoS:         mqtt.QoS0,
+					// Define the processing of the message handler.
+					Handler: func(topicName, message []byte) {
+						logger.Debug("full message", zap.ByteString("msg", message))
+						had := &HomeAssistantData{}
+						err := json.Unmarshal(message, had)
+						if err != nil {
+							logger.Error("Failed to unmarshall data", zap.Error(err))
 						}
-						sendData(vd)
-					}
-					logger.Debug("Data", zap.String("name", name), zap.String("value", dd.Svalue1))
-
+						name, stype := findVaderTypes(had.EventData.EntityID)
+						if name != "" {
+							valueFloat := had.EventData.NewState.Attributes["Temperature"].(float64)
+							if err != nil {
+								logger.Error("Could not parse float", zap.Any("value", had.EventData.NewState.Attributes["Temperature"]))
+								return
+							}
+							if name != "" {
+								vd := VaderData{
+									Name:       name,
+									Value:      valueFloat,
+									SensorType: stype,
+									Timestamp:  fmt.Sprintf("%d", time.Now().Unix()*1000),
+								}
+								sendData(vd)
+							} else {
+								logger.Debug("Found NO name")
+							}
+						}
+					},
 				},
 			},
-		},
-	})
+		})
+	}
+	if config.DataFormat == "domoticz" {
+		logger.Debug("Dataformat Domoticz")
+		// Subscribe to topics.
+		err = cli.Subscribe(&client.SubscribeOptions{
+			SubReqs: []*client.SubReq{
+				&client.SubReq{
+					TopicFilter: []byte("domoticz/out"),
+					QoS:         mqtt.QoS0,
+					// Define the processing of the message handler.
+					Handler: func(topicName, message []byte) {
+						logger.Debug("full message", zap.ByteString("msg", message))
+						dd := &DomoticzsData{}
+						err := json.Unmarshal(message, dd)
+						if err != nil {
+							logger.Error("Failed to unmarshall data", zap.Error(err))
+						}
+						name, stype := findVaderTypes(dd.Name)
+						valueFloat, err := strconv.ParseFloat(dd.Svalue1, 64)
+						if err != nil {
+							logger.Error("Could not parse float", zap.String("value", dd.Svalue1))
+							return
+						}
+						if name != "" {
+							vd := VaderData{
+								Name:       name,
+								Value:      valueFloat,
+								SensorType: stype,
+								Timestamp:  fmt.Sprintf("%d", time.Now().Unix()*1000),
+							}
+							sendData(vd)
+						}
+						logger.Debug("Data", zap.String("name", name), zap.String("value", dd.Svalue1))
+
+					},
+				},
+			},
+		})
+	}
+
 	if err != nil {
 		panic(err)
 	}
@@ -221,6 +273,7 @@ func createLogger() (*zap.Logger, zap.AtomicLevel, error) {
 
 func findVaderTypes(domoticz string) (string, string) {
 	for _, s := range config.Sensor {
+		fmt.Printf("%s, %s\n", s.Domoticz, domoticz)
 		if s.Domoticz == domoticz {
 			return s.Vader, s.SType
 		}
@@ -233,9 +286,9 @@ func sendData(vd VaderData) {
 	list = append(list, vd)
 	resp, err := resty.R().
 		SetBody(list).
-		Post("http://t.jpl.se/vader/rest/save")
+		Post("http://" + config.RemoteHost + "/" + config.RemoteApp + "/rest/save")
 	if err != nil {
-		logger.Error("Failed to post vader data", zap.Error(err))
+		logger.Error("Failed to post data", zap.Error(err))
 		return
 	}
 	if resp == nil {
